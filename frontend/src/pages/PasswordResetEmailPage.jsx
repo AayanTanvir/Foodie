128f2f69cab7d6e9113a9ef8a66e04b76a1f3b7c
AayanTanvir <k98778376@gmail.com>
2025-04-30 16:54:52 +0500

    Refactor password reset UI

--- frontend/src/pages/PasswordResetEmailPage.jsx
+++ frontend/src/pages/PasswordResetEmailPage.jsx
@@ -44,12 +44,14 @@ const PasswordResetEmailPage = () => {
 
     return (
         <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center flex-col">
-          <h1 className='mb-6 text-2xl text-neutral-800 font-medium'>Enter Your Email</h1>
-          <form onSubmit={submitEmail} className="w-1/4 bg-neutral-100 border-0 rounded flex flex-col items-center p-6 gap-4">
-              <input type="email" name="email" placeholder="Email" className="p-2 border rounded w-full outline-none"/>
-              <input type="submit" className="p-2 bg-neutral-800 text-white w-full cursor-pointer"/>
-              <p className='text-red-600'>{authError}</p>
-          </form>
+            <div className='w-1/4 h-fit'>
+                <h1 className='mb-6 text-3xl text-neutral-800 font-medium font-notoserif'>Reset Password</h1>
+            </div>
+            <form onSubmit={submitEmail} className="w-1/4 border-0 rounded flex flex-col items-start gap-4">
+                <input type="email" name="email" placeholder="Email" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+                <input type="submit" className="p-2 mt-2 bg-neutral-800 text-white w-full cursor-pointer"/>
+                <p className='text-red-600'>{authError}</p>
+            </form>
         </div>
     )
 }
--- frontend/src/pages/PasswordResetNewPasswordPage.jsx
+++ frontend/src/pages/PasswordResetNewPasswordPage.jsx
@@ -45,13 +45,15 @@ const PasswordResetNewPasswordPage = () => {
 
     return (
         <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center flex-col">
-        <h1 className='mb-6 text-2xl text-neutral-800 font-medium'>Enter New Password</h1>
-        <form onSubmit={submitNewPassword} className="w-1/4 bg-neutral-100 border-0 rounded flex flex-col items-center p-6 gap-4">
-            <input type="password" name="password1" placeholder="New Password" className="p-2 border rounded w-full outline-none"/>
-            <input type="password" name="password2" placeholder="Confirm New Password" className="p-2 border rounded w-full outline-none"/>
-            <input type="submit" className="p-2 bg-neutral-800 text-white w-full cursor-pointer"/>
-            <p className='text-red-600'>{authError}</p>
-        </form>
+            <div className='w-1/4 h-fit'>
+                <h1 className='mb-6 text-3xl text-neutral-800 font-notoserif font-medium'>New Password</h1>
+            </div>
+            <form onSubmit={submitNewPassword} className="w-1/4 border-0 rounded flex flex-col items-start gap-4">
+                <input type="password" name="password1" placeholder="New Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+                <input type="password" name="password2" placeholder="Confirm New Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+                <input type="submit" className="p-2 mt-2 bg-neutral-800 text-white w-full cursor-pointer"/>
+                <p className='text-red-600'>{authError}</p>
+            </form>
         </div>
     )
 }
