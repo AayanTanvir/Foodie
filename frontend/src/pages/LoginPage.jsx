e2e549fff45062312b7c82e9d120bd3ebd27b4bc
AayanTanvir <k98778376@gmail.com>
2025-04-30 16:46:10 +0500

    Refactor Signup page UI

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -14,19 +14,21 @@ const LoginPage = () => {
     }, [user, navigate]);
 
     return (
-      <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center flex-col">
-        <div className='w-1/4 h-fit'>
-            <h1 className='mb-6 text-2xl text-neutral-800 font-medium text-left'>Login</h1>
+        <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center flex-col">
+            <div className='w-1/4 h-fit'>
+                <h1 className='mb-6 text-4xl font-notoserif text-neutral-800 font-medium text-left cursor-default'>Login</h1>
+            </div>
+            <form onSubmit={loginUser} className="relative w-1/4 border-0 rounded flex flex-col items-start gap-4">
+            <input type="email" name="email" placeholder="Email" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+            <input type="password" name="password" placeholder="Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+            <div className='w-full h-fit flex justify-between items-center mb-4'>
+                <Link className='text-sm text-neutral-500 border border-neutral-300 px-2 py-1 transition duration-150 ease-in hover:border-neutral-400 hover:text-neutral-600 font-hedwig' to='/reset-password'>Forgot Password</Link>
+                <Link to='/signup' className='text-neutral-500 text-sm font-hedwig px-2 py-1 border border-neutral-300 transition duration-150 ease-in hover:border-neutral-400 hover:text-neutral-600'>Signup</Link>
+            </div>
+            <input type="submit" className="p-2 bg-neutral-800 text-white w-full cursor-pointer font-hedwig"/>
+            <p className='text-red-600'>{authError}</p>
+            </form>
         </div>
-        <form onSubmit={loginUser} className="relative w-1/4 border-0 rounded flex flex-col items-start gap-4">
-          <input type="email" name="email" placeholder="Email" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500" required/>
-          <input type="password" name="password" placeholder="Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500" required/>
-          <Link className='text-sm text-neutral-500 mb-4 border border-neutral-300 px-2 py-1 transition duration-150 ease-in hover:border-neutral-400 hover:text-neutral-600' to='/reset-password'>Forgot Password</Link>
-          <input type="submit" className="p-2 bg-neutral-800 text-white w-full cursor-pointer"/>
-          <p className='text-red-600'>{authError}</p>
-          <p className='text-neutral-800'>Don't have an account? <Link to='/signup' className='text-neutral-800 underline'>Signup!</Link></p>
-        </form>
-      </div>
     );
 };
 
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -15,15 +15,16 @@ const SignupPage = () => {
 
     return (
       <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center flex-col">
-        <h1 className='mb-6 text-2xl text-neutral-800 font-medium'>Signup</h1>
-        <form onSubmit={signupUser} className="w-1/4 bg-neutral-100 border-0 rounded flex flex-col items-center p-6 gap-4">
-          <input type="email" name="email" placeholder="Email" className="p-2 border rounded w-full outline-none"/>
-          <input type="text" name="username" placeholder="Username" className="p-2 border rounded w-full outline-none"/>
-          <input type="password" name="password1" placeholder="Password" className="p-2 border rounded w-full outline-none"/>
-          <input type="password" name="password2" placeholder="Confirm Password" className="p-2 border rounded w-full outline-none"/>
-          <input type="submit" className="p-2 bg-neutral-800 text-white w-full cursor-pointer"/>
+        <div className='w-1/4 h-fit'>
+            <h1 className='mb-6 text-4xl font-notoserif text-neutral-800 font-medium text-left cursor-default'>Signup</h1>
+        </div>
+        <form onSubmit={signupUser} className="w-1/4 border-0 rounded flex flex-col items-start gap-4">
+          <input type="email" name="email" placeholder="Email" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+          <input type="text" name="username" placeholder="Username" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+          <input type="password" name="password1" placeholder="Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+          <input type="password" name="password2" placeholder="Confirm Password" className="p-2 border w-full outline-none transition-all duration-200 ease-in hover:border-neutral-500 font-hedwig" required/>
+          <input type="submit" className="p-2 mt-4 bg-neutral-800 text-neutral-100 w-full cursor-pointer font-hedwig"/>
           <p className='text-red-600'>{authError}</p>
-          <p className='mt-8 text-neutral-800'>Already have an account? <Link to='/login' className='text-blue-700'>Login!</Link></p>
         </form>
       </div>
     );
