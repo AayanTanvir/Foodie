367b00a9b3b2e0df564c6cab083c283a5214cea9
AayanTanvir <k98778376@gmail.com>
2025-05-25 20:07:07 +0500

    Redesign navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 import { CartContext } from '../context/CartContext';
@@ -10,53 +10,71 @@ const Navbar = () => {
     let {user, logoutUser, verifyEmail} = useContext(AuthContext);
     let { isCartEmpty, cartItems } = useContext(CartContext);
     const cartCount = cartItems.length
+    const [showProfileDropdown, setShowProfileDropdown] = useState(false);
 
     //verify email: <a className='hover:cursor-pointer transition hover:text-gray-300' onClick={() => verifyEmail("send")}>Verify Email</a>
 
-    const getPageNavLinkClass = (isActive) => {
-        return [
-          'relative transition hover:text-gray-300 cursor-pointer group',
-          'after:content-[""] after:absolute after:bottom-0 after:left-0',
-          'after:h-1 after:bg-gray-300 after:transition-all after:duration-300',
-          isActive ? 'text-gray-300 after:w-full' : 'after:w-0 hover:after:w-full',
-        ].join(' ');
-      };
-
     return (
-        <div className='w-full h-12 bg-neutral-800 flex flex-row justify-evenly text-center fixed top-0 left-0 gap-4 text-gray-50 z-50'>
-            <div className='font-poppins font-normal flex flex-row justify-center items-center h-full'>
-                <NavLink to='/' className='transition hover:text-gray-300 cursor-pointer'>Home</NavLink>
-            </div>
-            <div className='font-poppins font-normal flex flex-row justify-center items-center h-full'>
-                {user && (
-                    <>
-                        {isCartEmpty ? (
-                            <a className='relative w-fit h-full px-1 bg-neutral-700 flex justify-center items-center transition cursor-not-allowed'>
-                                <img src={disabled_shopping_bag} alt="Cart" className='w-6'/>
-                            </a>
-                        ) : (
-                            <NavLink to="/cart" className={({ isActive }) =>
-                                    `relative w-fit h-full px-1 bg-neutral-800
-                                    flex justify-center items-center transition hover:bg-neutral-900
-                                    ${isActive ? 'bg-neutral-900' : ''}`}>
-
-                                <img src={shopping_bag} alt="Cart" className='w-6'/>
-                                {!isCartEmpty && (
-                                    <div className="w-[0.9rem] h-[0.9rem] rounded-full bg-gray-200 absolute
-                                        top-[15%] right-0 flex justify-center items-center">
-                                        <div className='text-center rounded-full flex justify-center items-center'>
-                                            <p className='font-poppins text-neutral-800 text-[0.8rem]'>{cartCount}</p>
-                                        </div>
-                                    </div>
-                                )}
-                            </NavLink>
-                        )}
-                    </>
+        <>
+            <div className='w-full h-12 bg-neutral-800 flex justify-between items-center fixed top-0 left-0 px-8 text-neutral-100 z-50'>
+                {user ? (
+                    <NavLink to='/' className='font-poppins font-extrabold tracking-wider h-full w-fit transition hover:bg-neutral-900 flex justify-center items-center px-3 cursor-pointer'>Food<span className='text-neutral-400'>ie</span></NavLink>
+                ) : (
+                    <a className='font-poppins font-extrabold tracking-wider h-full w-fit transition flex justify-center items-center px-3 cursor-default'>Food<span className='text-neutral-400'>ie</span></a>
                 )}
-                {user ? <NavLink className='cursor-pointer mx-5 transition hover:text-gray-300 after:bg-gray-300;w-2' onClick={logoutUser}>Logout</NavLink> : <NavLink className='hover:cursor-pointer mx-5 transition hover:text-gray-300' to='/login'>Login</NavLink>}
-                {!user ? <NavLink className='cursor-pointer mx-5 transition hover:text-gray-300' to='/signup'>Signup</NavLink> : <></>}
+                <div className='flex justify-center items-center h-full'>
+                    {user ? (
+                        <>
+                            {isCartEmpty ? (
+                                <a className='relative w-fit h-full px-2 bg-neutral-700 flex justify-center items-center transition cursor-not-allowed'>
+                                    <img src={disabled_shopping_bag} alt="Cart" className='w-6'/>
+                                </a>
+                            ) : (
+                                <NavLink to="/cart" className={({ isActive }) =>
+                                        `relative w-fit h-full px-2 bg-neutral-800
+                                        flex justify-center items-center transition hover:bg-neutral-900
+                                        ${isActive ? 'bg-neutral-900' : ''}`}>
+
+                                    <img src={shopping_bag} alt="Cart" className='w-6'/>
+                                    {!isCartEmpty && (
+                                        <div className="w-[0.9rem] h-[0.9rem] rounded-full bg-gray-200 absolute
+                                            top-[15%] right-0 flex justify-center items-center">
+                                            <div className='text-center rounded-full flex justify-center items-center'>
+                                                <p className='font-poppins text-neutral-800 text-[0.8rem]'>{cartCount}</p>
+                                            </div>
+                                        </div>
+                                    )}
+                                </NavLink>
+                            )}
+                            <a onMouseEnter={() => { setShowProfileDropdown(true) }} className={`h-full w-fit transition hover:bg-neutral-900 ${showProfileDropdown && 'bg-neutral-900'} flex justify-center items-center px-3 cursor-pointer`}>
+                                <div className='w-4 h-4 bg-neutral-200 rounded-full'></div>
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink className={({ isActive }) => `font-poppins h-full w-fit transition hover:bg-neutral-900 ${isActive && 'bg-neutral-900'} flex justify-center items-center px-3 cursor-pointer`} to='/login'>Login</NavLink>
+                            <NavLink className={({ isActive }) => `font-poppins h-full w-fit transition hover:bg-neutral-900 ${isActive && 'bg-neutral-900'} flex justify-center items-center px-3 cursor-pointer`} to='/signup'>Signup</NavLink>
+                        </>
+                    )}
+                </div>
             </div>
-        </div>
+            {showProfileDropdown && (
+                <div onMouseLeave={() => { setShowProfileDropdown(false) }} className='absolute right-8 z-50 top-12 w-32 h-fit bg-neutral-800 flex flex-col justify-start items-center'>
+                    <div className='w-full h-fit flex justify-evenly items-center p-2 cursor-pointer transition hover:bg-neutral-900'>
+                        <div className='w-1 h-1 bg-neutral-100 rounded-full'></div>
+                        <NavLink className="font-poppins text-neutral-100 transition hover:bg-neutral-900">Profile</NavLink>
+                    </div>
+                    <div className='w-full h-fit flex justify-evenly items-center p-2 cursor-pointer transition hover:bg-neutral-900'>
+                        <div className='w-1 h-1 bg-neutral-100 rounded-full'></div>
+                        <NavLink className="font-poppins text-neutral-100 transition hover:bg-neutral-900">Orders</NavLink>
+                    </div>
+                    <div className='w-full h-fit flex justify-evenly items-center p-2 cursor-pointer transition hover:bg-neutral-900'>
+                        <div className='w-1 h-1 bg-neutral-100 rounded-full'></div>
+                        <NavLink onClick={() => { logoutUser() }} className="font-poppins text-neutral-100 transition hover:bg-neutral-900">Logout</NavLink>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
