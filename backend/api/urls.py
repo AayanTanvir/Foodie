9c9a4549cdd7011f630c5fe3571cff3d72ce247a
AayanTanvir <k98778376@gmail.com>
2025-06-08 17:11:59 +0500

    remove token blacklisting and rotation

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import *
 
 urlpatterns = [
-    #auth
+    
     path('token/', MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('password-reset/', PasswordResetAPIView.as_view()),
